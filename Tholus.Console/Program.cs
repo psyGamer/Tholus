5405222d8332a65dddaf64c3214e506d3ff35489
microlith57 <microlith57@gmail.com>
2024-12-31 22:13:31 +1300

    add minimal Cocona structure, as a starting point

---
+++ Tholus.Console/Program.cs
@@ -0,0 +1,14 @@
+using Cocona;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        CoconaApp.Run<Program>(args);
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Not yet implemented.");
+    }
+}
